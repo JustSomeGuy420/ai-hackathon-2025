e5980099ac5cd0cfb52ab41450aaa42724788c63
JustSomeGuy420 <109366653+JustSomeGuy420@users.noreply.github.com>
2025-10-19 15:48:42 -0500

    Change route to serve index.html for all paths

--- frontend/my-react-app/server.js
+++ frontend/my-react-app/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/random.text', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
