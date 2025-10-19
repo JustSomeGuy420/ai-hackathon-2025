6880ad4741286ede56a0f98f7902f699320c317b
JustSomeGuy420 <109366653+JustSomeGuy420@users.noreply.github.com>
2025-10-19 15:23:16 -0500

    Update route to handle all paths in server.js

--- frontend/my-react-app/server.js
+++ frontend/my-react-app/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
+app.get('/:path(*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
