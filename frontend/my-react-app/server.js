88d1901ce3493b48db8009be83c224d26d9639e2
JustSomeGuy420 <109366653+JustSomeGuy420@users.noreply.github.com>
2025-10-19 15:16:27 -0500

    Change static file directory from 'dist' to 'build'

--- frontend/my-react-app/server.js
+++ frontend/my-react-app/server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
