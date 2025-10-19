0cc32af2b1e21fd40383559808a044d6d99e10f6
JustSomeGuy420 <109366653+JustSomeGuy420@users.noreply.github.com>
2025-10-19 14:53:38 -0500

    Update server to serve from 'build' directory

--- frontend/my-react-app/server.js
+++ frontend/my-react-app/server.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 10000;
